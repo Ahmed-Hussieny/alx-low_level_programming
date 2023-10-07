ccbab16c2f36e19af8b2bfe89a9aec30c32e90a3
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2023-10-08 00:09:31 +0300

    first Commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start Point
+ *
+ * Return: 0 (success)
+ * Description : printf Line
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
