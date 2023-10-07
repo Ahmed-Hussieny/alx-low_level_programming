9c63b22309386e655d3cf1f03b65d8fde967d656
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2023-10-08 00:03:06 +0300

    first Commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start Point
+ *
+ * Return: 0 (success)
+ * Description : puts Line
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
