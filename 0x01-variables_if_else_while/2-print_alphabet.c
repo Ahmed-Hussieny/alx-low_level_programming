eb6e461790929ec9b238c28db72b74c888519857
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2023-10-10 05:19:52 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print if number is grather than 5 or
+ *        less than 6 or equal 0
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
